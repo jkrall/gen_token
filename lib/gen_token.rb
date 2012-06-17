2be223619cbc9b14fda8ef910664ad83e4806fcf
Joshua Krall <jkrall@groupon.com>
2012-06-17 05:03:22 -0700

    Rails 3.2 compatibility

--- lib/gen_token.rb
+++ lib/gen_token.rb
@@ -8,23 +8,24 @@ module GenToken
   module GenTokenClassMethods
     def gen_token(key, options = {})
       token_def = {:name => key, :type => :hex, :length => 16}.merge(options)
-      write_inheritable_array(:token_fields, [token_def])
+      self.token_fields = [token_def]
     end
   end
   
   module ActiveRecordInstanceMethods
     def generate_tokens
-      self.class.read_inheritable_attribute(:token_fields).each do |token|
+      self.class.token_fields.each do |token|
         name, type, length = token[:name], token[:type], token[:length]
-        self[token[:name]] = ActiveSupport::SecureRandom.send(:"#{token[:type]}", token[:length])
+        self[token[:name]] = SecureRandom.send(:"#{token[:type]}", token[:length])
         while !self.class.send(:"find_by_#{token[:name]}", self[token[:name]]).nil?
-          self[token[:name]] = ActiveSupport::SecureRandom.send(:"#{token[:type]}", token[:length])
+          self[token[:name]] = SecureRandom.send(:"#{token[:type]}", token[:length])
         end
       end
     end
   end
   
   def self.included(klass)
+    klass.send :class_attribute, :token_fields
     klass.extend GenTokenClassMethods
     if Object.const_defined?(:ActiveRecord)
       if klass < ActiveRecord::Base
